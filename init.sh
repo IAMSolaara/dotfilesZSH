5673fc2e7b16870b6439bad596f4e7a38079ce12
Lorenzo Cauli <lorecast162@freenas.mentalabs-hq.com>
2020-08-05 12:30:09 +0200

    Added fonts and checks for existing directories

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+FONTSDIR=${XDG_DATA_HOME:-$HOME/.local/share}/fonts
+[ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 ln -sf $PWD/.zshrc $HOME
 ln -sf $PWD/.p10k.zsh $HOME
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+[ ! -d $FONTSDIR ] && mkdir -p $FONTSDIR
+
+ln -sf $PWD/fonts/* $FONTSDIR/
+
+[ ! -d $ZSH_CUSTOM/themes/powerlevel10k ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+[ ! -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+[ ! -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
